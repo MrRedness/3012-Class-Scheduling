4c430af61d6983ae8a6e9d6f917a3a5f2d6cf0b5
karthik <karthiksing05@gmail.com>
2024-11-07 20:53:23 -0500

    first commit, doing first-fit-coloring now!

---
+++ graph.py
@@ -0,0 +1,27 @@
+
+class Graph:
+
+    """
+    Graph class with adjacency list and adding built in for convenience!
+    """
+
+    def __init__(self):
+        self.adjLst = dict()
+        self.nodeSet = set()
+
+    def add_node(self, node):
+        self.adjLst[node] = {}
+        self.nodeSet.add(node)
+
+    def add_edge(self, n1, n2):
+        if n1 not in self.adjLst:
+            raise Exception("Please add node, " + n1 + ", first before adding edges to it!")
+        if n2 not in self.adjLst:
+            raise Exception("Please add node, " + n2 + ", first before adding edges to it!")
+
+        self.adjLst[n1][n2] = True
+        self.adjLst[n2][n1] = True
+
+    def get_neighbors(self, node):
+        return [k for k in self.adjLst[node].keys() if self.adjLst[node][k]]
+
