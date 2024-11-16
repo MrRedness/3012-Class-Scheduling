e10b6b2549149faf04650a250220357e511d59b6
karthik <karthiksing05@gmail.com>
2024-11-16 12:41:28 -0500

    more progress but another failed test case in testSmall2.csv

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,6 @@ from data_gen import csv_to_classes_dict, csv_to_student_dict
 
 DEBUG_MODE = False
 
-
 def first_fit_adaptive(student_classes):
     """
     Applies first-fit-coloring to a group of students to identify the schedules for each student.
@@ -51,36 +50,43 @@ def first_fit_adaptive(student_classes):
                     continue
                 # lowestColor is set to the color with the minumum frequency
                 if frequencyMap[lowestColor] > val:
+                    secondLowest = lowestColor
                     lowestColor = key
                 # secondLowest is set to the color with the second minium frequency
                 elif frequencyMap[secondLowest] > val:
                     secondLowest = key
 
-            if lowestColor != -1:
+            if lowestColor == -1:
+                lowestColor = secondLowest
+            if (DEBUG_MODE):
+                print(f"Coloring node {node} with color {lowestColor}")
+                print(frequencyMap)
+                print(
+                    f"Frequency of lowest color {lowestColor} is {frequencyMap[lowestColor]}")
+                print(
+                    f"Frequency of second lowest color {secondLowest} is {frequencyMap[secondLowest]}")
+            coloring[node] = lowestColor
+            if frequencyMap[lowestColor] > 0:
                 if (DEBUG_MODE):
-                    print(f"Coloring node {node} with color {lowestColor}")
-                    print(
-                        f"Frequency of lowest color {lowestColor} is {frequencyMap[lowestColor]}")
-                coloring[node] = lowestColor
-                if frequencyMap[lowestColor] > 0:
-                    if (DEBUG_MODE):
-                        print(f"Creating dupe for {node}")
-                    duplicate = node + "_dup"
+                    print(f"Creating dupe for {node}")
+                duplicate = node + "_dup"
 
-                    for neighbor in graph.get_neighbors(node):
-                        if coloring[neighbor] == lowestColor:
-                            graph.remove_edge(node, neighbor)
-                            graph.add_edge(duplicate, neighbor)
+                for neighbor in graph.get_neighbors(node):
+                    if coloring[neighbor] == lowestColor:
+                        graph.remove_edge(node, neighbor)
+                        graph.add_edge(duplicate, neighbor)
 
-                            for class_list in student_classes:
-                                if (class_list.count(node) > 0 and class_list.count(neighbor) > 0):
-                                    class_list.remove(node)
-                                    class_list.append(duplicate)
+                        for class_list in student_classes:
+                            if (class_list.count(node) > 0 and class_list.count(neighbor) > 0):
+                                class_list.remove(node)
+                                for student_class in class_list:
+                                    graph.add_edge(duplicate, student_class)
+                                class_list.append(duplicate)
 
-                            # TODO do we need to add more connective edges here than just the lowest color?
+                        # TODO do we need to add more connective edges here than just the lowest color?
 
-                    coloring[duplicate] = -1
-                    dupsCreated = True
+                coloring[duplicate] = -1
+                dupsCreated = True
             if (DEBUG_MODE):
                 Graph.visualize(graph, coloring)
 
@@ -92,31 +98,6 @@ def first_fit_adaptive(student_classes):
     first_fit_coloring()
 
     return graph, coloring
-
-# Just a helper function that assigns each student to each coloring and returns
-# maybe a CSV with a reordered class size for each student? Also need to
-# interpret duplicates for this!
-# def apply_coloring_to_students(students, coloring):
-#     # TODO NEED THIS DONE!
-#     print(students)
-#     print(coloring)
-#     students
-#     pass
-
-# def create_node_dict(student_dict):
-#     node_dict = dict()
-#     for class_list in student_dict.values():
-#         for class_name in class_list:
-#             node_dict[class_name] = []
-
-#     for student, class_list in student_dict.items():
-#         for class_name in class_list:
-#             node_dict[class_name].append(student)
-
-
-#     return node_dict
-
-# Reorders the list of classes to match the coloring 
 
 from time import sleep
 def reorder_student_list(student_classes, coloring):
@@ -138,24 +119,21 @@ def reorder_student_list(student_classes, coloring):
         print("\n\n")
 
 if __name__ == "__main__":
-    student_dict = csv_to_student_dict("Generated\\testSmall.csv")
+    student_dict = csv_to_student_dict("Generated\\testSmall2.csv")
 
     print(student_dict)
     
     student_classes = list(student_dict.values())
-
 
     graph, coloring = first_fit_adaptive(student_classes)
 
     print("\n\n")
 
     print(coloring)
-        # print(graph.adjLst)
-    Graph.visualize(graph, coloring)
+    # print(graph.adjLst)
 
     print("\n\n")
 
     reorder_student_list(student_classes, coloring)
-
-    print(student_dict)
-
+    
+    Graph.visualize(graph, coloring)
